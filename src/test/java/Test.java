99a3b5a250d4ae1890fcd3a5e4b1aa953f378d54
tudor <tudormoldovanu@gmail.com>
2018-10-18 11:18:21 +0300

    commit

---
+++ src/test/java/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+    @org.junit.Test
+    public void test2()
+    {
+        assert(2==2);
+    }
+
+    @org.junit.Test
+    public void test3()
+    {
+        assert(1==3);
+    }
+}
